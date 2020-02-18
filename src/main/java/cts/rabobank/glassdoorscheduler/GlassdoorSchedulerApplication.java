b336312171dd461544e3fcb79f46ef38af993404
kalaivani <kalaivani@kalaivanis-MacBook-Pro.local>
2020-02-18 07:52:43 +0530

    inital commit

---
+++ src/main/java/cts/rabobank/glassdoorscheduler/GlassdoorSchedulerApplication.java
@@ -0,0 +1,13 @@
+package cts.rabobank.glassdoorscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassdoorSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassdoorSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cts/rabobank/glassdoorscheduler/GlassdoorSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package cts.rabobank.glassdoorscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassdoorSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
