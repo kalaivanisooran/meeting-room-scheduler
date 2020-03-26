5487dd85055b0a60238dae3ab1e3058e99052878
VidhyaSivasubramanian <59553376+VidhyaSivasubramanian@users.noreply.github.com>
2020-03-26 22:54:32 +0530

    excluding weekends changes done

--- src/main/java/cts/rabobank/glassdoorscheduler/entity/Booking.java
+++ src/main/java/cts/rabobank/glassdoorscheduler/entity/Booking.java
@@ -34,7 +34,7 @@ public class Booking implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@ManyToOne(cascade = CascadeType.ALL)
